49b3a0f18f15e6ca61e773c35d9eb1c369e4e6a5
Mac Mini 15 <macmini15@MCM-OS-115.local>
2022-03-29 09:54:01 -0700

    adds further work

--- test/pageobjects/checkboxes.page.js
+++ test/pageobjects/checkboxes.page.js
@@ -1,14 +1,21 @@
 const Page = require('./page');
 
 //Target
-//
+// Check box 1 
+// http://the-internet.herokuapp.com/checkboxes
 
 class checkBoxesPage extends Page {
 
     get checkbox1 () {
-        return $('');
+        return $('/html/body/div[2]/div/div/form/input[1]');
+        
+        // Xpath
         // //form/input[1]
         // /html/body/div[2]/div/div/form/input[1]
+    }
+    // target checked attribute
+    get checked1 () {
+        return $('');
     }
 
 
--- test/specs/checkboxes.spec.js
+++ test/specs/checkboxes.spec.js
@@ -4,7 +4,16 @@ describe('My if statement application', () => {
     it('should click checkbox1 if checkbox1 is unchecked', async () => {
         await checkBoxesPage.open();
         
-        await expect(checkbox1).toBeExisting();
-        
+        // target test
+        //await expect(checkBoxesPage.checkbox1).toBeExisting();
+
+        //notes
+        // await checkBoxesPage.checkbox1.getAttribute('checked')
+
+        if (await checkBoxesPage.checkbox1.getAttribute('checked')) {
+            //  this block of code will be executed if the condition is true
+        } else {
+            //  this block of code will be executed if the condition is false
+        }
     });
 });
